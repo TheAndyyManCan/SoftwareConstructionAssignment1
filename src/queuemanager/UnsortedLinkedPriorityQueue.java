eb64614e99704feadf2ba515661da91e0aceef42
Andy <amcdonald1307@protonmail.com>
2023-03-23 16:11:20 +0000

    having a nightmare with git

--- src/queuemanager/UnsortedLinkedPriorityQueue.java
+++ src/queuemanager/UnsortedLinkedPriorityQueue.java
@@ -135,21 +135,6 @@ public class UnsortedLinkedPriorityQueue<T> implements PriorityQueue<T> {
                 priorityNodeNext = priorityNode.getPointer();
                 priorityNodePrev.setPointer(priorityNodeNext);
 
-                //remove the priority node
-                priorityNodeNext = priorityNode.getPointer();
-                priorityNodePrev.setPointer(priorityNodeNext);
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-=======
-            }
-
->>>>>>> d1223b762fe420c1faa1b2bc8e23dad280bddd44
-=======
-
-            }
-
->>>>>>> d1223b762fe420c1faa1b2bc8e23dad280bddd44
         }
     }
 
